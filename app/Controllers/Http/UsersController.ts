962dfd4598d54f65ce77a1cf22caab28bbabe802
GuilhermeMacedo01 <guilhermemorais586@gmail.com>
2023-10-15 17:40:24 -0300

    15-10_edit

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,9 +3,22 @@ import User from   'App/Models/User'
 
 export default class UsersController {
     public async create({ view }: HttpContextContract){
-        return view.render('user/create')
+        return view.render('users/create')
     }
-    public async store({ view }: HttpContextContract){
-        return view.render('user/create')
+    public async store({auth, request, response }: HttpContextContract){
+        const email = request.input('email')
+        const password = request.input('password')
+        const confirm_password = request.input('confirm_password')
+
+        if(password != confirm_password){
+            
+            return response.redirect().toRoute('users.create')
+        }
+
+        const user = await User.create({ email, password })
+
+        await auth.use('web').login(user)
+
+        return response.redirect().toRoute('index')
     }
 }
